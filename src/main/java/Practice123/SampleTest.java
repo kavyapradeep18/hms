c235d2e030a1324e8e98ed9f2c44fccbc60a2200
dell <dell@mypc>
2022-11-22 13:23:02 -0500

    com1

---
+++ src/main/java/Practice123/SampleTest.java
@@ -0,0 +1,19 @@
+package Practice123;
+
+
+
+public class SampleTest 
+{
+
+
+	
+	   public void script1()
+	   {
+		   System.out.println("script1");  
+	   }
+	  
+	   public void script2()
+	   {
+		   System.out.println("script2");  
+	   }
+}
